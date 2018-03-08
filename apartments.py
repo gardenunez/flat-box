4c8efc0c500f674872d14821e95f4d7b4ddcf940
jorge jardines <gardenunez@gmail.com>
2018-03-08 17:36:42 +0100

    load random data end-point

--- apartments.py
+++ apartments.py
@@ -3,3 +3,8 @@ import psycopg2
 def get_apartments(db):
     return db.execute("SELECT * FROM apartments limit 10")
 
+def load_apartments(db):
+    return db.execute("INSERT INTO apartments(lat, lon, rooms, area) \
+                      SELECT random()::numeric, random()::numeric, (random()*10)::integer, \
+                             (random()*100)::numeric FROM generate_series (1,10);")
+
--- app.py
+++ app.py
@@ -5,14 +5,20 @@ app = Flask(__name__)
 app.json_encoder = FlatBoxJsonEncoder
 
 
-from apartments import get_apartments
+from apartments import get_apartments, load_apartments
 from db import PostgresDb
-from flask import jsonify
+from flask import jsonify, make_response
 CONNECTION_STR = 'postgresql://postgres:postgres@db/postgres'
 @app.route("/")
 def hello():
     db = PostgresDb(connection_str=CONNECTION_STR)
     return jsonify(get_apartments(db))
+
+@app.route("/", methods=['POST'])
+def load():
+    db = PostgresDb(connection_str=CONNECTION_STR)
+    load_apartments(db)
+    return make_response(jsonify(), 201)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, use_reloader=True)
